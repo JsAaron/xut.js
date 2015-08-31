368d7e4fce6c72dbf22f46ccb4bfa8848898a300
jsAaron <401545553@qq.com>
2015-08-31 20:49:47 +0800

    delete

--- project/stream.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var stream = fs.createReadStream('test.txt');
-var files = fs.readdirSync(process.cwd());
-files.forEach(function(file){
-
-	// console.log(file)
-})
-
-console.log(stream)
--- somepackage/lib/interface.js.js
+++
@@ -1,4 +0,0 @@
-exports = function  (argument) {
-	console.log("hello")
-	// body...
-}
