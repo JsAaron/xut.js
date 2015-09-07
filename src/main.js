a057e0cab6ed29f97c901e32fa66208f491047cb
jsAaron <401545553@qq.com>
2015-09-07 22:29:56 +0800

    bower

--- src/main.js
+++ src/main.js
@@ -1,24 +1,51 @@
-// With proper loader configuration you can load,
-// pre-process and insert css directly with require().
-// See webpack.config.js for details.  
 require('./main.styl')
 
-var Vue = require('vue')
-var app = new Vue({
-  el: '#app',
-  data: {
-    view: 'page-a'
-  },
-  components: {
-    // define the main pages as async components.
-    'page-a': function (resolve) {
-      require(['./views/a'], resolve)
-    },
-    'page-b': function (resolve) {
-      require(['./views/b'], resolve)
-    }
-  }
+// var Vue = require('vue')
+ 
+Vue.component('child', {
+    // 声明 props
+    props: ['msg'],
+    // 该 prop 可以在模板内部被使用，
+    // 也可以类似 `this.msg` 这样来赋值
+    template: '<span>{{msg}}</span>'
 })
+  
+
+  
+
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     view: 'page-a'
+//   },
+//   components: {
+//     // 页面1
+//     'page-a': function (resolve) {
+//       require(['./views/a'], resolve)
+//     },
+//     // 页面2
+//     'page-b': function (resolve) {
+//       require(['./views/b'], resolve)
+//     }
+//   }
+// })
+
+
+// Vue.directive('disable', function (value) {
+//     this.el.disabled = !!value
+// })
+
+// var vm = new Vue({
+//     el: "#demo",
+//     data: {
+//         disabled: false
+//     }
+// })
+
+// vm.watch('message',function(){
+//   console.log(arguements)
+// })
+
 
 /**
  * Some really crude routing logic here, just for
@@ -28,9 +55,9 @@ var app = new Vue({
  * automatically handle all the lazy loading for us.
  */
 
-function route () {
-  app.view = window.location.hash.slice(1) || 'page-a'
-}
+// function route () {
+//   app.view = window.location.hash.slice(1) || 'page-a'
+// }
 
-window.addEventListener('hashchange', route)
-window.addEventListener('load', route)
+// window.addEventListener('hashchange', route)
+// window.addEventListener('load', route)
