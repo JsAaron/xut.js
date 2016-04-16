6eb03e9e253ca6dc84f873e7d0b1529681067ac0
JsAaron <401545553@qq.com>
2016-04-16 18:34:27 +0800

    1

--- gulpfile.js
+++ gulpfile.js
@@ -92,3 +92,79 @@ gulp.task('develop', ['database', 'rollup-pack', 'server'], function() {
         gulp.run('rollup-pack');
     });
 })
+
+
+
+/**
+ * 替换所有反斜杠为斜杠
+ * @return {[type]} [description]
+ */
+var escape = function(str) {
+    var strs = new Array();
+    var a = str;
+    strs = a.split("");
+    for (var i = 0; i < strs.length; i++) {
+        a = a.replace("\\", "/")
+    }
+    return a;
+}
+
+
+//打包
+gulp.task('pack-command', function(callback) {
+    fs.readFile('./index.html', "utf8", function(error, data) {
+        if (error) throw error;
+        var arr = []
+        var path;
+        var cwdPath = escape(process.cwd())
+
+        var command = []
+        command.push('cd ' + cwdPath + '/lib')
+        command.push('\n')
+        command.push('java -jar ..\\build\\compiler.jar ')
+
+        var scripts = data.match(/<script.*?>.*?<\/script>/ig);
+        scripts.forEach(function(val) {
+            val = val.match(/lib.*?.js/);
+            if (val && val.length) {
+                path = cwdPath + "/" + val[0]
+                command.push(path + ' ')
+            }
+        })
+
+        command.push('--js_output_file=..\\build\\_file\\xxtppt.js')
+        command.push('\n')
+        command.push('cd ' + cwdPath + '/build')
+        command.push('\n')
+        command.push('Pause')
+
+        command = command.join('');
+
+        fs.writeFile('build/xxtppt.bat', command, function(err) {
+            console('youxi!');
+        });
+
+    });
+})
+
+gulp.task('pack', function(callback) {
+    fs.readFile('./index.html', "utf8", function(error, data) {
+        if (error) throw error;
+        var arr = []
+        var path;
+        var data
+        var cwdPath = escape(process.cwd())
+
+        fs.writeFileSync('xxtppt.js', '')
+
+        var scripts = data.match(/<script.*?>.*?<\/script>/ig);
+        scripts.forEach(function(val) {
+            val = val.match(/lib.*?.js/);
+            if (val && val.length) {
+                path = cwdPath + "/" + val[0]
+                fs.appendFileSync('xxtppt.js', fs.readFileSync(path))
+            }
+        })
+
+    });
+})
