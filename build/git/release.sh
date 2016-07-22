2fc472e7785aac27fef97f28c9627e90ac26f3a7
aaron <401545553@qq.com>
2016-07-22 15:11:07 +0800

    { 2015.7.15 Compel common spriteAnimation to advancedSpriteAnimation}

--- build/git/release.sh
+++ build/git/release.sh
@@ -33,12 +33,3 @@ git commit -m "{$content}"
 git remote add origin https://github.com/JsAaron/es6-magazine.git
 git push origin master
 
-
-# echo "git diff ? (defalut no. input 'yes',if want to it )"
-# read PERSON
-# if [$next -eq $PERSON]
-# then
-#     echo 'aaa'
-# else
-#    git diff
-# fi
