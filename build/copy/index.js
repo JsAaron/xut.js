6cfa5ce09257bd11715f8383673f4e397b5f3d23
aaron <401545553@qq.com>
2016-08-08 15:04:50 +0800

    add jsWidgetPath config for miaomiaoxue, fix sprite animation

--- build/copy/index.js
+++ build/copy/index.js
@@ -4,48 +4,47 @@ const fsextra = require('fs-extra')
 const cp = require('child_process')
 const _ = require("underscore");
 const createRE = require('./filter')
-
-const src = '.'
-const dist = '/Users/mac/project/git/es6-magazine/'
-
 const filterRE = createRE()
 
 //./build/.config.js
 //./build/dev/test.js
 //build/dev/webpack.dev.conf.js
 const segmentation = new RegExp("[.]?\\w+([.]?\\w*)*", "ig")
-const excludeRE = new RegExp(".git|node_modules|README.md|README.gif", "ig")
+const excludeRE = new RegExp("epub|安装说明|.git|node_modules|README", "ig")
 
-console.log(
-    '【Regular filter】\n' +
-    filterRE +
-    '\n'
-)
+// console.log(
+//     '【Regular filter】\n' +
+//     filterRE +
+//     '\n'
+// )
 
-let count = 0
-
-
-var files = fs.readdirSync(dist);
-for (file of files) {
-    if (!excludeRE.test(file)) {
-        fsextra.removeSync(dist + file)
+const copy = function(src, dist) {
+    console.log('dist: ' + dist)
+    let count = 0
+    let files = fs.readdirSync(dist);
+    for (file of files) {
+        if (!excludeRE.test(file)) {
+            fsextra.removeSync(dist + file)
+        }
     }
-}
-
-const ls = (ff) => {
-    var files = fs.readdirSync(ff);
-    for (fn in files) {
-        var rootPath = ff + path.sep
-        var filename = rootPath + files[fn]
-        var stat = fs.lstatSync(filename);
-        if (stat.isDirectory() == true) {
-            ls(filename)
-        } else {
-            if (!filterRE.test(rootPath)) {
-                ++count
-                fsextra.copySync(filename, dist + filename)
+    let ls = (ff) => {
+        var files = fs.readdirSync(ff);
+        for (fn in files) {
+            var rootPath = ff + path.sep
+            var filename = rootPath + files[fn]
+            var stat = fs.lstatSync(filename);
+            if (stat.isDirectory() == true) {
+                ls(filename)
+            } else {
+                if (!filterRE.test(rootPath)) {
+                    ++count
+                    fsextra.copySync(filename, dist + filename)
+                }
             }
         }
     }
+    ls(src)
 }
-ls(src)
+
+copy('.', '/Users/mac/project/git/es6-magazine/')
+copy('.', '/Users/mac/project/svn/server/magazine-develop/assets/www/')
