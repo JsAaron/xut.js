9cb9c7d05fdd7db5e5960fac08edcae6af7870a5
JsAaron <401545553@qq.com>
2016-09-19 22:18:03 +0800

    Proportion

--- src/lib/config/index.js
+++ src/lib/config/index.js
@@ -15,8 +15,10 @@ import {
     getLayerMode,
     fixProportion
 }
-from './fix'
+from './set'
 
+import improtMode from './mode'
+import improtData from './data'
 
 const plat = Xut.plat
 const isIphone = Xut.plat.isIphone
@@ -213,127 +215,6 @@ Xut.log = function(info, name) {
 _.extend(config, {
 
     /**
-     * 页面可视模式
-     * 2016.9.19
-     * 3种分辨率显示模式:
-     * 默认全屏缩放
-     * 0 永远100%屏幕尺寸自适应
-     * 1 按照宽度正比缩放,高度正比缩放，居中(适应画轴拼接模式)
-     * 2 宽度100% 自适应高度，居中
-     * 3 高度100% 自适应宽度，居中,溢出隐藏
-     * @type {Number}
-     */
-    visualMode: 0,
-
-    /**
-     * 画轴模式
-     * 在不同分辨率下，按照正比缩放拼接
-     * 在一个可视区中，可以看到3个li拼接后的效果
-     * [scrollPaintingMode description]
-     * @type {Boolean}
-     */
-    scrollPaintingMode: false,
-
-    /**
-     * 虚拟模式
-     * 采用word排版，如果是横屏的布局放到竖版的手机上
-     * 就需要分割排版布局
-     * 1:画轴模式默认横版ppt,竖版默认启动虚拟模式分栏
-     * @type {Boolean}
-     */
-    virtualMode: false,
-
-    /**
-     * 全局翻页模式
-     * 数据库定义的翻页模式
-     * 用来兼容客户端的制作模式
-     * 妙妙学模式处理，多页面下翻页切换
-     * 0 通过滑动翻页
-     * 1 (锁掉滑动),直接快速切换页面(通过左右按键快速切换页面)
-     * @type {Number}
-     */
-    flipMode: 0,
-
-    /**
-     * 调试模式
-     * 如果启动桌面调试模式,自动打开缓存加载,就是每次都打开都回到最后看到的一页
-     * [debugMode description]
-     * @type {Boolean}
-     */
-    debugMode: false,
-
-    /**
-     * 独立canvas模式处理
-     * 为了测试方便
-     * 可以直接切换到dom模式
-     * @type {Boolean}
-     */
-    onlyDomMode: false,
-
-    /**
-     *canvas的处理模式
-     *合并模式：merge
-     *单个模式：single
-     */
-    canvasProcessMode: 'merge',
-
-    /**
-     * 直接通过数据库的历史记录定位到指定的页面
-     * Xut.View.LoadScenario({
-     *     'scenarioId' : scenarioInfo[0],
-     *     'chapterId'  : scenarioInfo[1],
-     *     'pageIndex'  : scenarioInfo[2]
-     *  })
-     *  {
-     *     'scenarioId' : 7,
-     *     'chapterId'  : 9
-     *  }
-     * @type {Boolean}
-     */
-    deBugHistory: false,
-
-    /**
-     * 支持电子在在线阅读,向服务端取数据
-     * 自定义配置地址即可'
-     * @type {String}
-     */
-    onlineModeUrl: 'lib/data/database.php',
-
-    /**
-     * 2016.7.26
-     * 读酷增加强制插件模式
-     * [isPlugin description]
-     * @type {Boolean}
-     */
-    isPlugin: window.DUKUCONFIG && plat.isIOS,
-
-
-    /**
-     * 数据库名
-     * @type {[type]}
-     */
-    dbName: window.xxtmagzinedbname || 'magazine',
-
-    /**
-     * 存储模式
-     * 0 APK应用本身
-     * 1 外置SD卡
-     */
-    storageMode: 0,
-
-    /**
-     * 应用路径唯一标示
-     * @type {[type]}
-     */
-    appId: null,
-
-    /**
-     * 资源路径
-     * @type {[type]}
-     */
-    pathAddress: null,
-
-    /**
      * 视频文件路径
      * @return {[type]} [description]
      */
@@ -419,7 +300,8 @@ _.extend(config, {
      * @type {Number}
      */
     dbSize: 1
-});
+
+}, improtMode, improtData)
 
 
 Xut.config = config
@@ -484,6 +366,3 @@ export function initConfig() {
      */
     setProportion()
 }
-
-
-
---
+++ src/lib/config/mode.js
@@ -0,0 +1,78 @@
+/**
+ * 模式配置
+ */
+export default {
+
+    /**
+     * 页面可视模式
+     * 2016.9.19
+     * 3种分辨率显示模式:
+     * 默认全屏缩放
+     * 0 永远100%屏幕尺寸自适应
+     * 1 按照宽度正比缩放,高度正比缩放，居中(适应画轴拼接模式)
+     * 2 宽度100% 自适应高度，居中
+     * 3 高度100% 自适应宽度，居中,溢出隐藏
+     * @type {Number}
+     */
+    visualMode: 0,
+
+    /**
+     * 画轴模式
+     * 在不同分辨率下，按照正比缩放拼接
+     * 在一个可视区中，可以看到3个li拼接后的效果
+     * [scrollPaintingMode description]
+     * @type {Boolean}
+     */
+    scrollPaintingMode: false,
+
+    /**
+     * 虚拟模式
+     * 采用word排版，如果是横屏的布局放到竖版的手机上
+     * 就需要分割排版布局
+     * 1:画轴模式默认横版ppt,竖版默认启动虚拟模式分栏
+     * @type {Boolean}
+     */
+    virtualMode: false,
+
+    /**
+     * 全局翻页模式
+     * 数据库定义的翻页模式
+     * 用来兼容客户端的制作模式
+     * 妙妙学模式处理，多页面下翻页切换
+     * 0 通过滑动翻页
+     * 1 (锁掉滑动),直接快速切换页面(通过左右按键快速切换页面)
+     * @type {Number}
+     */
+    flipMode: 0,
+
+    /**
+     * 调试模式
+     * 如果启动桌面调试模式,自动打开缓存加载,就是每次都打开都回到最后看到的一页
+     * [debugMode description]
+     * @type {Boolean}
+     */
+    debugMode: false,
+
+    /**
+     * 独立canvas模式处理
+     * 为了测试方便
+     * 可以直接切换到dom模式
+     * @type {Boolean}
+     */
+    onlyDomMode: false,
+
+    /**
+     * 直接通过数据库的历史记录定位到指定的页面
+     * Xut.View.LoadScenario({
+     *     'scenarioId' : scenarioInfo[0],
+     *     'chapterId'  : scenarioInfo[1],
+     *     'pageIndex'  : scenarioInfo[2]
+     *  })
+     *  {
+     *     'scenarioId' : 7,
+     *     'chapterId'  : 9
+     *  }
+     * @type {Boolean}
+     */
+    deBugHistory: false
+}
