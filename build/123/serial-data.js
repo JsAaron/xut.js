4bfd8301a5b16150aae92f556806c8f136ef423e
aaron <401545553@qq.com>
2016-09-22 16:48:48 +0800

    1

---
+++ build/123/serial-data.js
@@ -0,0 +1,56 @@
+const sqlite = require('../sqlite/index')
+const fs = require('fs');
+const _ = require("underscore")
+const fsextra = require('fs-extra')
+const watch = require('gulp-watch');
+
+const GALLERY = 'gallery'
+const WIDGET = 'widget'
+const DBNAME = 'xxtebook.db'
+
+const addMonitor = () => {
+    // watch(xxtebook, () => {
+    //     console.log('【xxtebook.db is change!】')
+    //     sqlite.resolve(() => {
+    //         console.log('【new xxtebook.db is complete!】')
+    //     })
+    // })
+}
+
+const get = function(paths) {
+    if (!paths.length) {
+        console.log('数据解析完毕')
+        return
+    }
+    let path = paths.shift()
+    if (!fs.existsSync(path + '/SQLResult.js')) {
+        console.log('【SQLResult.js not available!】')
+        sqlite.resolve(path, path + '/' + DBNAME, function() {
+            get(paths)
+        })
+    } else {
+        get(paths)
+    }
+}
+
+
+module.exports = (conf, spinner) => {
+    const path = conf.srcDir + 'content/'
+    const files = fs.readdirSync(path)
+    const arr = []
+    const parse = []
+    _.each(files, function(file) {
+        var stat = fs.lstatSync(path + file);
+        if (stat.isDirectory()) {
+            if (file == GALLERY || file == WIDGET) {} else {
+                parse.push(path + file)
+            }
+        } else {
+            if (file === DBNAME) {
+                parse.push(path)
+            }
+        }
+    })
+    get(parse)
+    addMonitor()
+}
