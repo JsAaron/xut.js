8070659cd685be9ddc8e3789952faf7106886903
JsAaron <401545553@qq.com>
2016-09-24 00:12:14 +0800

    add translation.js

---
+++ src/lib/manager/mediator.js
@@ -0,0 +1,406 @@
+/*********************************************************************
+ *              场景容器构造器
+ *          1 构件页面级容器
+ *          2 翻页全局事件
+ *
+ **********************************************************************/
+
+import { Observer } from '../observer/index'
+import GlobalEvent from '../swipe/index.js'
+import { Dispatcher } from './dispatcher/index'
+import { delegateHooks } from './hooks'
+import { filterProcessor } from './filter'
+import overrideApi from '../dynamic-api'
+
+//定义访问器
+import {
+    defProtected as def,
+    defAccess
+} from '../util/index'
+
+
+/**
+ * 配置多页面参数
+ * @return {[type]} [description]
+ */
+const configMultiple = (options) => {
+    //如果是epub,强制转换为单页面
+    if (Xut.IBooks.Enabled) {
+        options.multiplePages = false
+    } else {
+
+        //判断多页面情况
+        //1 数据库定义
+        //2 系统优化
+        options.multiplePages =
+            options.flipMode
+            ? options.flipMode
+            : options.pageMode ? true : false
+    }
+}
+
+
+/**
+ * 判断处理那个页面层次
+ * 找到pageType类型
+ * 项目分4个层
+ * page mater page浮动 mater浮动
+ * 通过
+ * 因为冒泡的元素，可能是页面层，也可能是母板上的
+ * @return {Boolean} [description]
+ */
+const isBelong = (target) => {
+    var pageType = 'page';
+    if (target.dataset && target.dataset.belong) {
+        pageType = target.dataset.belong;
+    }
+    return pageType
+}
+
+
+/**
+ * 阻止元素的默认行为
+ * 在火狐下面image带有href的行为
+ * 会自动触发另存为
+ * @return {[type]} [description]
+ *
+ * 2016.3.18
+ * 妙妙学 滚动插件默认行为被阻止
+ *
+ * 2016.7.26
+ * 读库强制PC模式了
+ */
+const preventDefault = (evtObj, target) => {
+    //var tagName = target.nodeName.toLowerCase();
+    if (Xut.plat.isBrowser && !Xut.IBooks.Enabled && !window.MMXCONFIG && !window.DUKUCONFIG) {
+        evtObj.preventDefault && evtObj.preventDefault();
+    }
+}
+
+
+class Mediator extends Observer {
+
+    constructor(parameter) {
+
+        super()
+ 
+        const vm = this
+
+        //配置文件
+        const options = vm.options = _.extend({
+            //翻页模式
+            //根据页码数决定,主要是优化一些代码
+            //true  是多页面模式,支持翻页滑动
+            //false 单页面模式,不能翻页，只能跳转
+            'pageMode': false,
+
+            //是否多场景加载
+            //单页场景 false
+            //多场景   true
+            'multiScenario': false,
+
+            //是否为连续页面
+            //通过pageMode的参数定义
+            'multiplePages': false
+        }, parameter, {
+            flipMode: Xut.config.flipMode
+        })
+
+        //配置多页面参数
+        configMultiple(options)
+
+        const $globalEvent = vm.$globalEvent = new GlobalEvent(options);
+        const $dispatcher = vm.$dispatcher = new Dispatcher(vm);
+
+        //如果是主场景,才能切换系统工具栏
+        if (options.multiplePages) {
+            this.addTools(vm)
+        }
+
+        //事件句柄对象
+        let handlerObj = null;
+
+        /**
+         * 过滤器.全局控制函数
+         * return true 阻止页面滑动
+         */
+        $globalEvent.$watch('onFilter', (hookCallback, point, evtObj) => {
+            let target = point.target;
+            //阻止默认行为
+            preventDefault(evtObj, target);
+            //页面类型
+            let pageType = isBelong(target);
+            //根节点
+            let parentNode = $globalEvent.findRootElement(point, pageType);
+            //执行过滤处理
+            handlerObj = filterProcessor.call(parentNode, point, pageType);
+            //停止翻页,针对content对象可以拖动,滑动的情况处理
+            if (!handlerObj || handlerObj.attribute === 'disable') {
+                hookCallback();
+            }
+        });
+
+
+        /**
+         * 触屏滑动,通知pageMgr处理页面移动
+         * @return {[type]} [description]
+         */
+        $globalEvent.$watch('onMove', (data) => {
+            $dispatcher.move(data)
+        });
+
+
+        /**
+         * 触屏松手点击
+         * 无滑动
+         */
+        $globalEvent.$watch('onTap', (pageIndex, hookCallback) => {
+            if (handlerObj) {
+                if (handlerObj.handlers) {
+                    handlerObj.handlers(handlerObj.elem, handlerObj.attribute, handlerObj.rootNode, pageIndex);
+                } else {
+                    if (!Xut.Contents.Canvas.getIsTap()) {
+                        vm.$emit('change:toggleToolbar')
+                    }
+                }
+                handlerObj = null;
+                hookCallback();
+            }
+        });
+
+
+        /**
+         * 触屏滑动,通知ProcessMgr关闭所有激活的热点
+         * @return {[type]}          [description]
+         */
+        $globalEvent.$watch('onUpSlider', (pointers) => {
+            $dispatcher.suspend(pointers)
+        });
+
+
+        /**
+         * 翻页动画完成回调
+         * @return {[type]}              [description]
+         */
+        $globalEvent.$watch('onComplete', (direction, pagePointer, unfliplock, isQuickTurn) => {
+            $dispatcher.complete(direction, pagePointer, unfliplock, isQuickTurn)
+        });
+
+
+        /**
+         * 切换页面
+         * @return {[type]}      [description]
+         */
+        $globalEvent.$watch('onJumpPage', (data) => {
+            $dispatcher.jumpPage(data);
+        });
+
+
+        /**
+         * 退出应用
+         * @return {[type]}      [description]
+         */
+        $globalEvent.$watch('onDropApp', (data) => {
+            window.GLOBALIFRAME && Xut.publish('magazine:dropApp');
+        });
+
+
+        /**
+         * 母板移动反馈
+         * 只有存在data-parallaxProcessed
+         * 才需要重新激活对象
+         * 删除parallaxProcessed
+         */
+        $globalEvent.$watch('onMasterMove', (hindex, target) => {
+            if (/Content/i.test(target.id) && target.getAttribute('data-parallaxProcessed')) {
+                $dispatcher.masterMgr && $dispatcher.masterMgr.reactivation(target);
+            }
+        });
+
+        vm.$overrideApi();
+    }
+
+
+    /**
+     * 系统工具栏
+     */
+    addTools(vm) {
+
+        _.extend(delegateHooks, {
+
+            /**
+             * li节点,多线程创建的时候处理滑动
+             */
+            'data-container' () {
+                vm.$emit('change:toggleToolbar')
+            },
+
+            /**
+             * 是背景层
+             */
+            'data-multilayer' () {
+                //改变工具条状态
+                vm.$emit('change:toggleToolbar')
+            },
+
+            /**
+             * 默认content元素可以翻页
+             */
+            'data-behavior' (target, attribute, rootNode, pageIndex) {
+                //没有事件的元素,即可翻页又可点击切换工具栏
+                if (attribute == 'click-swipe') {
+                    vm.$emit('change:toggleToolbar')
+                }
+            }
+        })
+
+    }
+
+}
+
+
+var medProto = Mediator.prototype
+
+
+/**
+ * 是否多场景模式
+ */
+defAccess(medProto, '$multiScenario', {
+    get: function() {
+        return this.options.multiScenario
+    }
+});
+
+
+/**
+ * 动态注入对象接口
+ * 注入对象管理,注册所有widget组件对象
+ *  content类型  创建时注册
+ *  widget类型   执行时注册
+ *  widget 包括 视频 音频 Action 子文档 弹出口 类型
+ *  这种类型是冒泡处理，无法传递钩子，直接用这个接口与场景对接
+ */
+defAccess(medProto, '$injectionComponent', {
+    set: function(regData) {
+        var injection;
+        if (injection = this.$dispatcher[regData.pageType + 'Mgr']) {
+            injection.abstractAssistPocess(regData.pageIndex, function(pageObj) {
+                pageObj.baseRegisterComponent.call(pageObj, regData.widget);
+            })
+        } else {
+            console.log('注册injection失败,regData=' + regData)
+        }
+    }
+});
+
+/**
+ * 得到当前的视图页面
+ * @return {[type]}   [description]
+ */
+defAccess(medProto, '$curVmPage', {
+    get: function() {
+        return this.$dispatcher.pageMgr.abstractGetPageObj(this.$globalEvent.getHindex());
+    }
+});
+
+
+/**
+ *  监听viewmodel内部的状态的改变,触发后传入值
+ *
+ *  与状态有关的change:
+ *      翻页
+ *          'flipOver' : function(pageIndex) {},
+ *
+ *      切换工具栏
+ *          'toggleToolbar' : function(state, pointer) {},
+ *
+ *      复位工具栏
+ *          'resetToolbar'  : function() {},
+ *
+ *      隐藏下一页按钮
+ *          'hideNext'   : function(state) {},
+ *
+ *      显示下一页按钮
+ *          'showNext'   : function() {}
+ *
+ *  与创建相关
+ *      创建完毕回调
+ *          'createComplete': null,
+ *      创建后中断自动运行回调
+ *          'suspendAutoCallback': null
+ *
+ */
+def(medProto, '$bind', function(key, callback) {
+    var vm = this
+    vm.$watch('change:' + key, function() {
+        callback.apply(vm, arguments)
+    })
+})
+
+
+/**
+ * 创建页面
+ * @return {[type]} [description]
+ */
+def(medProto, '$init', function() {
+    this.$dispatcher.initCreate();
+});
+
+
+/**
+ * 运动动画
+ * @return {[type]} [description]
+ */
+def(medProto, '$run', function() {
+    var vm = this;
+    vm.$dispatcher.pageMgr.activateAutoRuns(
+        vm.$globalEvent.getHindex(), Xut.Presentation.GetPageObj()
+    )
+});
+
+
+/**
+ * 复位对象
+ * @return {[type]} [description]
+ */
+def(medProto, '$reset', function() {
+    return this.$dispatcher.pageMgr.resetOriginal(this.$globalEvent.getHindex());
+});
+
+
+/**
+ * 停止所有任务
+ * @return {[type]} [description]
+ */
+def(medProto, '$suspend', function() {
+    Xut.Application.Suspend({
+        skipMedia: true //跨页面不处理
+    })
+});
+
+
+/**
+ * 销毁场景内部对象
+ * @return {[type]} [description]
+ */
+def(medProto, '$destroy', function() {
+    this.$off();
+    this.$globalEvent.destroy();
+    this.$dispatcher.destroy();
+    this.$dispatcher = null;
+    this.$globalEvent = null;
+});
+
+
+/**
+ * 设置所有API接口
+ * @return {[type]} [description]
+ */
+def(medProto, '$overrideApi', function() {
+    overrideApi(this)
+})
+
+
+export {
+    Mediator
+}
---
+++ src/lib/pagebase/move/parallax.js
@@ -0,0 +1,148 @@
+
+const transitionDuration = Xut.style.transitionDuration
+const transform = Xut.style.transform
+const setTranslateZ = Xut.style.setTranslateZ
+
+const hasValue = function(value) {
+    return value != undefined
+}
+
+/**
+ * transform转化成相对应的偏移量
+ */
+export function _transformConversion(property, distance, nodes) {
+    var temp = {},
+        i;
+
+    for (i in property) {
+        switch (i) {
+            case 'translateX':
+            case 'translateZ':
+                temp[i] = distance * nodes * property[i];
+                break;
+            case 'translateY':
+                temp[i] = distance * (config.viewSize.height / config.viewSize.width) * nodes * property[i]
+                break;
+            case 'opacityStart':
+                temp[i] = property.opacityStart;
+                break;
+            default:
+                //乘以-1是为了向右翻页时取值为正,位移不需这样做
+                temp[i] = -1 * distance / config.viewSize.width * property[i] * nodes;
+        }
+    }
+    return temp;
+}
+
+
+
+/**
+ * 移动叠加值
+ * @param  {[type]} property       [description]
+ * @param  {[type]} repairProperty [description]
+ * @return {[type]}                [description]
+ */
+export function _flipMove(property, repairProperty) {
+    var temp = {};
+    var start = property.opacityStart;
+    for (var i in property) {
+        temp[i] = property[i] + repairProperty[i];
+    }
+    if (start > -1) temp.opacityStart = start;
+    return temp;
+}
+
+
+/**
+ * 翻页结束
+ * @param  {[type]} property       [description]
+ * @param  {[type]} repairProperty [description]
+ * @return {[type]}                [description]
+ */
+export function _flipOver(property, repairProperty) {
+    return _flipMove(property, repairProperty);
+}
+
+
+/**
+ * 反弹
+ * @param  {[type]} property       [description]
+ * @param  {[type]} repairProperty [description]
+ * @return {[type]}                [description]
+ */
+export function _flipRebound(property, repairProperty) {
+    var temp = {};
+    for (var i in property) {
+        temp[i] = repairProperty[i] || property[i];
+    }
+    return temp;
+}
+
+
+/**
+ * 结束后缓存上一个记录
+ * @param  {[type]} property       [description]
+ * @param  {[type]} repairProperty [description]
+ * @return {[type]}                [description]
+ */
+export function _overMemory(property, repairProperty) {
+    for (var i in property) {
+        repairProperty[i] = property[i];
+    }
+}
+
+
+/**
+ * 变化节点的css3transform属性
+ * @param  {[type]} rootNode     [description]
+ * @param  {[type]} speed        [description]
+ * @param  {[type]} property     [description]
+ * @param  {[type]} opacityStart [description]
+ * @return {[type]}              [description]
+ */
+export function _transformNodes(rootNode, speed, property, opacityStart) {
+    var style = {},
+        effect = '',
+        x = 0,
+        y = 0,
+        z = 0,
+        round = Math.round;
+
+    if (hasValue(property.translateX) || hasValue(property.translateY) || hasValue(property.translateZ)) {
+        x = round(property.translateX) || 0;
+        y = round(property.translateY) || 0;
+        z = round(property.translateZ) || 0;
+        const translateZ = setTranslateZ(z)
+        effect += `translate(${x}px,${y}px) ${translateZ}`
+    }
+
+    if (hasValue(property.rotateX) || hasValue(property.rotateY) || hasValue(property.rotateZ)) {
+        x = round(property.rotateX)
+        y = round(property.rotateY)
+        z = round(property.rotateZ)
+        effect += x ? 'rotateX(' + x + 'deg) ' : ''
+        effect += y ? 'rotateY(' + y + 'deg) ' : ''
+        effect += z ? 'rotateZ(' + z + 'deg) ' : ''
+    }
+
+    if (hasValue(property.scaleX) || hasValue(property.scaleY) || hasValue(property.scaleZ)) {
+        x = round(property.scaleX * 100) / 100 || 1;
+        y = round(property.scaleY * 100) / 100 || 1;
+        z = round(property.scaleZ * 100) / 100 || 1;
+        effect += String.format('scale3d({0},{1},{2}) ', x, y, z);
+    }
+
+    if (property.opacity != undefined) {
+        style.opacity = round(property.opacity * 100) / 100 + opacityStart;
+        effect += ';'
+    }
+
+    ////////////////
+    //最终改变视觉对象的坐标 //
+    ////////////////
+    if (effect) {
+        style[transitionDuration] = speed + 'ms';
+        style[transform] = effect;
+        rootNode.css(style);
+    }
+}
