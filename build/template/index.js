43a6d59cb31557bed1520616a055ead941f581c1
401545553 <401545553@qq.com>
2016-11-20 09:02:21 +0800

    add sql.js copy

--- build/template/index.js
+++ build/template/index.js
@@ -1,16 +1,20 @@
-
 const killOccupied = require('../kill.occupied')
+const fsextra = require('fs-extra')
 const browserSync = require("browser-sync");
 const convertSVG = require('../convert.svg')
 const serialData = require('../serial.data')
 
 const prot = 8000
 const rootPath = './apk/'
+const sqlPath = rootPath + 'content/SQLResult.js'
+const targetSqlPath =  rootPath + 'datacache/SQLResult.js'
 
 killOccupied(prot, () => {
 
-	convertSVG(rootPath)
-	serialData(rootPath)
+    convertSVG(rootPath)
+    serialData(rootPath)
+
+    fsextra.copySync(sqlPath, targetSqlPath)
 
     browserSync({
         port: prot,
