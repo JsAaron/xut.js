f9d699442014ddba951e307c4a30612607d0df5d
401545553 <401545553@qq.com>
2016-11-29 15:41:51 +0800

    v872.2 2016.11.29 fix scroll.area.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -7,7 +7,7 @@ import { setDelay, disable } from './initialize/busy.cursor'
 import nextTick from './util/nexttick'
 import init from './initialize/index'
 
-Xut.Version = 872.1
+Xut.Version = 872.2
 
 if (Xut.plat.isBrowser) {
     //Mobile browser automatically broadcast platform media processing
