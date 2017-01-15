8b977e35b36996017c221fc06958e2ae8867c091
aaron <401545553@qq.com>
2017-01-15 20:38:23 +0800

    Merge files

--- src/lib/flowtest/compiler.js
+++
@@ -1,7 +0,0 @@
-/**
- * Size.js
- * @flow
- */
-export function add(num1: number, num2: string): number {
-  return num1;
-};
--- src/lib/flowtest/test.js
+++
@@ -1,5 +0,0 @@
-/** @flow */
-
-import { add } from "./compiler.js";
-
-var two: number = add(1,1);
