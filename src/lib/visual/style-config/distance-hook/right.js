157dab99b33f53f23603e1269a6e6ed93e5bbc5d
aaron <401545553@qq.com>
2017-01-19 11:38:22 +0800

    no message

---
+++ src/lib/visual/style-config/distance-hook/right.js
@@ -0,0 +1,78 @@
+/************************
+ * right页面钩子
+ * distance -1 -2 -3 -N 递减
+ *************************/
+export const rightPageHook = {
+    flipMove: {
+        prev() {},
+        /**
+         * 右滑动
+         * distance -1 -> -N 递减
+         */
+        next(distance, pageStyles) {
+
+            let middlePageStyle = pageStyles.middle
+            let rightPageStyle = pageStyles.right
+
+            //中间：溢出
+            if (middlePageStyle && middlePageStyle.visualLeftInteger) {
+                //右边：溢出
+                if (rightPageStyle && rightPageStyle.visualLeftInteger) {
+                    return distance + rightPageStyle.visualWidth
+                }
+                //右边：正常
+                else {
+                    return distance + middlePageStyle.visualWidth - middlePageStyle.visualLeftInteger
+                }
+            }
+            //中间：正常
+            else {
+                //右边：溢出
+                if (rightPageStyle && rightPageStyle.visualLeftInteger) {
+                    return distance + middlePageStyle.visualWidth + rightPageStyle.visualLeftInteger
+                }
+                //右边：正常
+                else {
+                    return distance + rightPageStyle.visualWidth
+                }
+            }
+        }
+    },
+    flipRebound: {
+        prev() {},
+        next(distance, pageStyles) {
+
+            let middlePageStyle = pageStyles.middle
+            let rightPageStyle = pageStyles.right
+
+            //中间：溢出
+            if (middlePageStyle && middlePageStyle.visualLeftInteger) {
+                //右边：溢出
+                if (rightPageStyle && rightPageStyle.visualLeftInteger) {
+                    return rightPageStyle.visualWidth
+                }
+                //右边：正常
+                else {
+                    return middlePageStyle.visualWidth - middlePageStyle.visualLeftInteger
+                }
+            }
+            //中间：正常
+            else {
+                //右边：溢出
+                if (rightPageStyle && rightPageStyle.visualLeftInteger) {
+                    return middlePageStyle.visualWidth + rightPageStyle.visualLeftInteger
+                }
+                //右边：正常
+                else {
+                    return middlePageStyle.visualWidth
+                }
+            }
+        }
+    },
+    flipOver: {
+        prev() {},
+        next() {
+            return 0
+        }
+    }
+}
