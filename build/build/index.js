08cf5632493b9bd201daf70b25d664d3b5635937
jsAaron <401545553@qq.com>
2017-08-25 22:39:42 +0800

    versionPromise

--- build/build/index.js
+++ build/build/index.js
@@ -1,15 +1,12 @@
 const compileRollup = require('../rollup.base.conf')
-
 const compileExternal = require('../external.script')
 const compileJs = require('./compile.js')
 const compileCSS = require('./compile.css')
 const compileVersion = require('./compile.version')
 const buildName = process.argv[process.argv.length - 1] || 'webpack-full-dev'
-const config = require('../config')(buildName)
+const config = require('../config')(buildName);
 
-
-async function compileRelease() {
-
+(async() => {
   await compileRollup({
     entry: config.entry,
     banner: config.banner,
@@ -24,10 +21,10 @@ async function compileRelease() {
     externalFiles: config.externalFiles
   })
 
-  await compileJs(config, scriptUrls)
-  await compileCSS(config)
-  await compileVersion(config)
-}
-
-compileRelease()
-
+  const jsPromise = compileJs(config, scriptUrls)
+  const cssPromise = compileCSS(config)
+  const versionPromise = compileVersion(config)
+  await jsPromise
+  await cssPromise
+  await versionPromise
+})();
